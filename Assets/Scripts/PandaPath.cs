dc33997e5ea7a34d122ac4905c413a0b80d14d7d
songjinp <jinpengjps@hotmail.com>
2016-05-01 23:40:32 +1200

    Initial commit added all Assist
    Currently the base is my version of assignment 2, WIP merging with
    others assignment 2

---
+++ Assets/Scripts/PandaPath.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PandaPath : MonoBehaviour {
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    public GameObject playerFox;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+     
+        float range = Vector3.Distance(this.transform.position,playerFox.transform.position);
+   
+        if (range < 7)
+        {
+            agent.SetDestination(playerFox.transform.position);
+        }
+        if (agent.remainingDistance < 0.5f) { GotoNextPoint(); }
+    }
+}
