7733f82025eebe48cbf912ae5620a8c66a78dc53
Jono Mingard <reason.koan@gmail.com>
2016-05-05 17:50:35 +1200

    Add axis input script and create example number input

---
+++ Assets/Scripts/UI/AxisInput.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+[System.Serializable]
+public class AxisMovedEvent : UnityEvent<float> { }
+
+public class AxisInput : MonoBehaviour {
+	public string axis;
+	public AxisMovedEvent onMove;
+
+	private bool awaitingInput = true;
+	private float lastPosition = 0;
+
+	void Start() {
+		if (onMove == null) onMove = new AxisMovedEvent();
+	}
+
+	void Update() {
+		float currentPosition = Input.GetAxis(axis);
+
+		if (awaitingInput) {
+			checkInput(currentPosition);
+		} else {
+			checkReverseMovement(currentPosition);
+		}
+
+		lastPosition = currentPosition;
+	}
+
+	private void checkInput(float position) {
+		if (Mathf.Abs(position) > Mathf.Abs(lastPosition)) {
+			awaitingInput = false;
+			onMove.Invoke(position);
+		}
+	}
+
+	private void checkReverseMovement(float position) {
+		if (Mathf.Abs(position) < Mathf.Abs(lastPosition)) {
+			awaitingInput = true;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/SetPlayerCount.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetPlayerCount : MonoBehaviour {
+	public int minValue = 2;
+	public int maxValue = 4;
+
+	public int value { get; private set; }
+
+	private Text text;
+
+	void Start() {
+		value = minValue;
+		text = GetComponentInChildren<Text>();
+		text.text = value.ToString();
+	}
+
+	public void OnInput(float axisPosition) {
+		if (axisPosition > 0 && value < maxValue) {
+			value++;
+		} else if (axisPosition < 0 && value > minValue) {
+			value--;
+		}
+		text.text = value.ToString();
+	}
+}
