4476525881e123273c06ebe208c01199f40604ca
songjinp <jinpengjps@hotmail.com>
2016-05-28 17:31:22 +1200

    Change powerCreater now only two powerup can exist at a time

--- Assets/Scripts/PowerUp/PowerUpCreater.cs
+++ Assets/Scripts/PowerUp/PowerUpCreater.cs
@@ -7,25 +7,51 @@ public class PowerUpCreater : MonoBehaviour {
     public GameObject PowerUpObj;
     public float SpawnTime = 4f;
     private float TimeTicker;
+	public GameObject[] existedPowerUp;
 
-    void start()
+    void Start()
     {
         TimeTicker = SpawnTime;
     }
+	void Awake () {
+		existedPowerUp = new GameObject[SpawnPostion.Length];
+	}
+
 	void Update () {
         if (!inGameScene.inProgress) {
             return;
         }
+
         TimeTicker -= Time.deltaTime;
-        if(TimeTicker < 0.0f)
+		if(TimeTicker < 0.0f && checkTotalPowerup() != 2)
         {
             TimeTicker = SpawnTime;
-            int randomPower = Random.Range(0, SpawnPostion.Length);
-            Instantiate(PowerUpObj, SpawnPostion[randomPower].position, SpawnPostion[randomPower].rotation);
+			int randomPower = findEmptySpawnPostion();
+			existedPowerUp[randomPower] = (GameObject)Instantiate(PowerUpObj, 
+			SpawnPostion[randomPower].position, SpawnPostion[randomPower].rotation);
         }
-        /*foreach (Transform t in SpawnPostion)
-        {
-            Instantiate(PowerUpObj, t.position, t.rotation);
-        }*/
+       
     }
+
+	private int checkTotalPowerup(){
+		int count = 0;
+		foreach (GameObject g in existedPowerUp) {
+			if (g != null) {
+				count++;
+			} 
+		}
+		return count;
+	}
+
+	private int findEmptySpawnPostion(){
+		ArrayList index = new ArrayList ();
+		for (int i = 0; i < existedPowerUp.Length; i++) {
+			if (existedPowerUp[i] == null) {
+				index.Add (i);
+			}
+		}
+		int randomPower = Random.Range(0, index.Count);
+		return (int)(index[randomPower]);
+	}
+
 }
