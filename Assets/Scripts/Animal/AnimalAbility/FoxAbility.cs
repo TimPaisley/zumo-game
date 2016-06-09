1971173a7aa20d826f4801ffd04f7810f2b73fcf
songjinp <jinpengjps@hotmail.com>
2016-06-09 15:08:28 +1200

    Updated the effect when fox ability is in use

--- Assets/Scripts/Animal/AnimalAbility/FoxAbility.cs
+++ Assets/Scripts/Animal/AnimalAbility/FoxAbility.cs
@@ -34,23 +34,56 @@ public class FoxAbility : MonoBehaviour,AnimalAbility {
 
 				print ("Disabled Fox ability");
 
+			} else {
+				Color newColour = getNextColor(rend.material.GetColor("_Color"));
+				foreach(Material m in rend.materials){
+					m.color = newColour;
+				}
 			}
 		} else {
 			return;
 		}
 	}
 
+	private Color getNextColor(Color currentColor)
+	{
+		float velocity = 0.05f;
+		float max = 1.0f;
+		float min = 0.0f;
+		float r = currentColor.r;
+		float g = currentColor.g;
+		float b = currentColor.b;
+		if (r >= max)
+		{
+			if (b > min) { return new Color(max, min, b - velocity); } // decreasing previous
+			else if (g < max) { return new Color(max, g + velocity, min); } //increase next
+			else { return new Color(max - velocity, max, min); } // next one increased, decrease this one once and hand over to g==1
+		}
+		else if (g >= max)
+		{
+			if (r > min) { return new Color(r - velocity, max, min); }
+			else if (b < max) { return new Color(min, g, b + velocity); }
+			else { return new Color(min, max - velocity, max); }
+
+		}
+		else if (b >= 1)
+		{
+			if (g > min) { return new Color(min, g - velocity, max); }
+			else if (r <max) { return new Color(r + velocity, min, max); }
+			else { return new Color(max, min, max - velocity); }
+		}
+		return new Color(max, min, min);
+	}
+
+
 	public void applyAbility (){
 		if (isAvailable) {
 			print ("Fox Ability");
-
+			Color newColour = getNextColor(rend.material.GetColor("_Color"));
 			foreach(Material m in rend.materials){
-				Color color = new Color(m.color.a/2, m.color.r, m.color.g, m.color.b);
-				//color.a = 0.1f;
-				m.color = color;
-				//m.color = Color.clear;
-
+				m.color = newColour;
 			}
+			this.transform.tag = "Untagged";
 			isActive = true;
 		}
 		isAvailable = false;
