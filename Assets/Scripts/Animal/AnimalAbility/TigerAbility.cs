efa59719adf27acd95fa76850b90d2177d78a4fa
songjinp <jinpengjps@hotmail.com>
2016-06-12 16:15:25 +1200

    Added WIP Tiger ability

--- Assets/Scripts/Animal/AnimalAbility/TigerAbility.cs
+++ Assets/Scripts/Animal/AnimalAbility/TigerAbility.cs
@@ -3,12 +3,47 @@ using System.Collections;
 
 public class TigerAbility : MonoBehaviour,AnimalAbility {
 
-	// Use this for initialization
-	void Start () {
-	
+    //private Rigidbody rb;
+	private AnimalController animal;
+	private bool isAvailable = true;
+	private bool isActive = false;
+	public float ticker = 10.0f;
+	private SkinnedMeshRenderer rend;
+	public Color[] colors;
+    public GameObject head;
+	//SkinnedMeshRenderer smr;
+	void Start(){
+		//rb = this.GetComponent<Rigidbody>();
+		animal = GetComponent<AnimalController>();
+		rend = GetComponentInChildren<SkinnedMeshRenderer>();  
+		colors = new Color[rend.materials.Length];	
+		for (int i = 0; i < rend.materials.Length; i++) {
+			colors [i] = rend.materials [i].color;
+		}
 	}
-	
-	public void applyAbility(){
-		print ("Tiger Ability");
+
+	void Update () {
+		if (isActive) {
+			ticker -= Time.deltaTime;
+			if (ticker < 0.0f) {
+				isActive = false;
+
+                print ("Disabled Tiger ability");
+			} 
+		} else {
+			return;
+		}
+	}
+
+
+
+	public void applyAbility (){
+		if (isAvailable) {
+            print("Tiger Ability");
+
+            
+             isActive = true;
+		}
+		isAvailable = false;
 	}
 }
