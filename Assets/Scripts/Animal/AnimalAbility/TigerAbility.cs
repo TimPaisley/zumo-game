70fcb147a2d53fa2ed709ce6f7186775e4d496b1
songjinp <jinpengjps@hotmail.com>
2016-06-13 15:05:27 +1200

    Added particle system for tiger ability

--- Assets/Scripts/Animal/AnimalAbility/TigerAbility.cs
+++ Assets/Scripts/Animal/AnimalAbility/TigerAbility.cs
@@ -7,18 +7,12 @@ public class TigerAbility : MonoBehaviour,AnimalAbility {
 	private bool isAvailable = true;
 	private bool isActive = false;
 	public float ticker = 10.0f;
-	private SkinnedMeshRenderer rend;
-	public Color[] colors;
-    public bool flash = false;
+	public ParticleSystem tigerPS;
+	private ParticleSystem.EmissionModule tigerEM;
 
 	void Start(){
-		//rb = this.GetComponent<Rigidbody>();
 		animal = GetComponent<AnimalController>();
-		rend = GetComponentInChildren<SkinnedMeshRenderer>();  
-		colors = new Color[rend.materials.Length];	
-		for (int i = 0; i < rend.materials.Length; i++) {
-			colors [i] = rend.materials [i].color;
-		}
+		tigerEM = tigerPS.emission;
 	}
 
 	void Update () {
@@ -26,37 +20,24 @@ public class TigerAbility : MonoBehaviour,AnimalAbility {
 			ticker -= Time.deltaTime;
 			if (ticker < 0.0f) {
 				isActive = false;
-				for (int i = 0; i < rend.materials.Length; i++) {
-					rend.materials [i].color = colors [i];
-				}
+				tigerEM.enabled = false;
+				tigerPS.Stop ();
+				tigerPS.Clear ();
                 animal.tigerAbility = false;
                 print ("Disabled Tiger ability");
-			} else {
-				Color newColour = getNextColor(rend.material.GetColor("_Color"));
-				foreach(Material m in rend.materials){
-					m.color = newColour;
-				}
 			}
 		} else {
 			return;
 		}
 	}
-
-	private Color getNextColor(Color currentColor)
-	{
-        return Color.black;
-    }
-
-
+		
 	public void applyAbility (){
 		if (isAvailable) {
 			print ("Tiger Ability");
-			Color newColour = getNextColor(rend.material.GetColor("_Color"));
-			foreach(Material m in rend.materials){
-				m.color = newColour;
-			}
+			tigerPS.Simulate(0.0f,true,true);
+			tigerEM.enabled = true;
+			tigerPS.Play ();
             animal.tigerAbility = true;
-          
             isActive = true;
 		}
 		isAvailable = false;
