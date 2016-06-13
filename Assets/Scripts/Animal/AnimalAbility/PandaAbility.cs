95e12e8a6f3f93507b23e7b51337f92b1b478bef
songjinp <jinpengjps@hotmail.com>
2016-06-13 14:43:31 +1200

    Fixed panda ability and added new animation

--- Assets/Scripts/Animal/AnimalAbility/PandaAbility.cs
+++ Assets/Scripts/Animal/AnimalAbility/PandaAbility.cs
@@ -9,10 +9,12 @@ public class PandaAbility : MonoBehaviour, AnimalAbility {
 	public float ticker = 10.0f;
 	private SkinnedMeshRenderer rend;
 	public Color[] colors;
+	private Animator animator;
 
 	void Start(){
-	  //rb = this.GetComponent<Rigidbody>();
-	  animal = GetComponent<AnimalController>();
+	    animator = GetComponent<Animator> ();
+	    //rb = this.GetComponent<Rigidbody>();
+	    animal = GetComponent<AnimalController>();
 		rend = GetComponentInChildren<SkinnedMeshRenderer>();  
 		colors = new Color[rend.materials.Length];	
 		for (int i = 0; i < rend.materials.Length; i++) {
@@ -27,10 +29,11 @@ public class PandaAbility : MonoBehaviour, AnimalAbility {
 				isActive = false;
 				animal.pandaAbility = false;
 				animal.disableControl = false;
+				animator.SetBool("usingAbility", false);
 
-				for (int i = 0; i < rend.materials.Length; i++) {
-					rend.materials [i].color = colors [i];
-				}
+//				for (int i = 0; i < rend.materials.Length; i++) {
+//					rend.materials [i].color = colors [i];
+//				}
 
 				print ("Disabled Panda ability");
 
@@ -45,12 +48,13 @@ public class PandaAbility : MonoBehaviour, AnimalAbility {
 			print ("Panda ability");
 			animal.pandaAbility = true;
 			animal.disableControl = true;
-			foreach(Material m in rend.materials){
-				//m.color = Color.gray;
-				Color color = new Color(m.color.a/2, m.color.r, m.color.g, m.color.b);
-				//color.a = 0.1f;
-				m.color = color;
-			}
+			animator.SetBool("usingAbility", true);
+//			foreach(Material m in rend.materials){
+//				//m.color = Color.gray;
+//				Color color = new Color(m.color.a/2, m.color.r, m.color.g, m.color.b);
+//				//color.a = 0.1f;
+//				m.color = color;
+//			}
 			isActive = true;
 		}
 		isAvailable = false;
