2918f88fb2d77bb0099713e00c01c562ea25bbd0
songjinp <jinpengjps@hotmail.com>
2016-06-13 16:28:10 +1200

    Added Panther and lion ability

---
+++ Assets/Scripts/Animal/AnimalAbility/LionAbility.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LionAbility : MonoBehaviour,AnimalAbility {
+
+	private AnimalController animal;
+	private bool isAvailable = true;
+
+	void Start(){
+		animal = GetComponent<AnimalController>();
+	}
+
+	void Update () {
+		
+	}
+
+	public void applyAbility (){
+		if (isAvailable) {
+			print ("Lion Ability");
+
+		}
+		isAvailable = false;
+	}
+}
---
+++ Assets/Scripts/Animal/AnimalAbility/PantherAbility.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PantherAbility : MonoBehaviour,AnimalAbility {
+
+	private bool isAvailable = true;
+	private bool isActive = false;
+	public float ticker = 5.0f;
+
+	private float originalPostion;
+	private CameraManager cm;
+	public Camera blackscreen;
+
+	void Update () {
+		Vector3 camPostion;
+		if (isActive) {
+			ticker -= Time.deltaTime;
+			if (ticker < 0.0f) {
+				isActive = false;
+				blackscreen.gameObject.SetActive (false);
+				print ("Disabled Panther ability");
+			} 
+		}
+	}
+
+	public void applyAbility (){
+		cm = FindObjectOfType<CameraManager> ();
+		originalPostion = cm.mainCamera.transform.localPosition.x;
+		if (isAvailable) {
+			print ("Panther Ability");
+			isActive = true;
+			blackscreen.gameObject.SetActive (true);
+		}
+		isAvailable = false;
+	}
+}
