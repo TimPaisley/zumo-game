2fc793065f3a2bf74fbc154eb6c01f319aa192af
Jono Mingard <reason.koan@gmail.com>
2016-07-29 17:22:47 +1200

    Animate animals when chosen

--- Assets/Scripts/New/Menus/ChoosableAnimal.cs
+++ Assets/Scripts/New/Menus/ChoosableAnimal.cs
@@ -3,10 +3,15 @@ using UnityEngine.UI;
 
 namespace Zumo {
 	public class ChoosableAnimal : MonoBehaviour {
+        const float CHOSEN_JUMP_MULTIPLIER = 300;
+
 		public Image selectionIndicator;
 		public Animal animal;
 
 		GameState state;
+        Animator animator;
+        Rigidbody rigidBody;
+
 		Vector2 basePosition;
 
 		public Player player { get; private set; }
@@ -16,9 +21,17 @@ namespace Zumo {
 		}
 
 		void Awake () {
+            // Find and create the components we need
 			state = FindObjectOfType<GameManager>().state;
+            animator = GetComponentInChildren<Animator>();
 
-			animal.transform.SetParent(null);
+            gameObject.CopyComponent(animal.GetComponent<BoxCollider>(), new string[] { "material", "center", "size" });
+            rigidBody = gameObject.CopyComponent(animal.GetComponent<Rigidbody>(), new string[] { "mass" });
+
+            animator.enabled = false;
+
+            // Set up dependent objects
+            animal.transform.SetParent(null);
 			animal.gameObject.SetActive(false);
 			DontDestroyOnLoad(animal.gameObject);
 
@@ -40,13 +53,18 @@ namespace Zumo {
             var playerNameText = selectionIndicator.GetComponentInChildren<Text>();
             playerNameText.text = player.shortName;
             playerNameText.color = player.color;
+
+            animator.enabled = true;
+            rigidBody.AddForce(Vector3.up * rigidBody.mass * CHOSEN_JUMP_MULTIPLIER);
 		}
 
 		public void ResetChoice () {
 			player = null;
 
 			selectionIndicator.gameObject.SetActive(false);
-		}
+
+            animator.enabled = false;
+        }
 
 		public float DistanceFrom (Vector2 point) {
 			return Vector2.Distance(point, basePosition);
---
+++ Assets/Scripts/New/Util/ComponentHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Zumo {
+    public static class ComponentHelper {
+        public static T CopyValuesFrom<T> (this Component component, T other, string[] values) where T : Component {
+            var type = component.GetType();
+            if (type != other.GetType()) return null; // type mis-match
+
+            var flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+            
+            foreach (var pinfo in type.GetProperties(flags)) {
+                if (pinfo.CanWrite && values.Contains(pinfo.Name)) {
+                    try {
+                        pinfo.SetValue(component, pinfo.GetValue(other, null), null);
+                    } catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+                }
+            }
+            
+            foreach (var finfo in type.GetFields(flags)) {
+                if (values.Contains(finfo.Name)) {
+                    finfo.SetValue(component, finfo.GetValue(other));
+                }
+            }
+
+            return component as T;
+        }
+
+        public static T CopyComponent<T> (this GameObject gameObject, T toAdd, string[] fieldsToCopy) where T : Component {
+            return gameObject.AddComponent<T>().CopyValuesFrom(toAdd, fieldsToCopy) as T;
+        }
+    }
+}
